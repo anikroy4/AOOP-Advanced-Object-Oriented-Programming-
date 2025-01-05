abb591e117df186c2798f855fd4b18ca2b16b570
Anik Roy <141570698+anikroy4@users.noreply.github.com>
2025-01-05 06:36:34 +0600

    project

---
+++ Job-Portal/JobPortal/db/hosted_db.md
@@ -0,0 +1,1 @@
+[Link](https://console.clever-cloud.com/users/me/addons/addon_509e0f99-8f71-4175-be3c-7bf66d6b6659)
---
+++ Job-Portal/JobPortal/src/com/jobportal/application/models/DB_VARIABLES.java
@@ -0,0 +1,22 @@
+package com.jobportal.application.models;
+
+public interface DB_VARIABLES {
+    public static String 
+    DB="bliztv7vdsvg8awvb6ub",
+    HOST="bliztv7vdsvg8awvb6ub-mysql.services.clever-cloud.com",
+    PASSWORD="Q1sQuFt8xuPunfU34HqC",
+    PORT="3306",
+    URI="mysql://uez5rqwvmr8dct7x:Q1sQuFt8xuPunfU34HqC@bliztv7vdsvg8awvb6ub-mysql.services.clever-cloud.com:3306/bliztv7vdsvg8awvb6ub",
+    USER="uez5rqwvmr8dct7x",
+    VERSION="8.0";
+
+
+    // public static String 
+    // DB="job_portal",
+    // HOST="localhost",
+    // PASSWORD="",
+    // PORT="3306",
+    // USER="root",
+    // VERSION="8.0";
+    
+}
